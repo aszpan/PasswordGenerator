2b22de8ab0e4fea1af08695c8a155f973e2e47c0
Ashley Szpanelewski <ashleyszp@gmail.com>
2022-08-03 16:48:39 -0400

    Added prompts under GeneratePassword f(x) and put them toLower in JS file

--- script.js
+++ script.js
@@ -12,10 +12,63 @@
 // 4.) Generate password
 
 // Variables
-//if we use split function for the criteria, we can use concat later to combine
-//the users requested criteria (if needed)
-var passLowercase = [azertyuiopqsdfghjklmwxcvbn];
-var passUppercase = [AZERTYUIOPQSDFGHJKLMWXCVBN];
+var generateBtn = document.querySelector("#generate");
+var passLowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var passUppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 var passNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var passSymbols = [
   "!",
@@ -41,54 +94,53 @@ var passSymbols = [
   "~",
 ];
 
-var passwordLength = window.prompt(
-  "How long would you like your password to be? (Must be a number between 8 and 128)"
-);
-
-var lowercase = window.prompt("Does your password require lowercase letters? (Y/N)");
-var uppercase = window.prompt("Does your password require uppercase letters? (Y/N)");
-var numbers = window.prompt("Does your password require numbers? (Y/N)");
-var symbols = window.prompt("Does your password require symbols? (Y/N)");
-
-
-
 function generatePassword() {
-  //need something to concat if prompt === y
-
-
-  switch(criteria) {
-  case lowercase:
-    newpassword += passLowercase;
-    break;
-  case uppercase:
-    break;
-    
-  case numbers:
-    break;
-  case symbols:
-    break;
-  default:
-    alert("Enter at least one criteria");
-}
-
-  //to validate if password is between 8 ands 128
-  if (passwordLength.length < 8 || passwordLength.length > 128) {
+  //have to use prompts per assignment rubric
+  var passwordLength = window.prompt(
+    "How long would you like your password to be? (Must be a number between 8 and 128)"
+  );
+  //parse int takes a string and changes it to int
+  parseInt(passwordLength);
+  //to validate if password is between 8 ands 128. may need to move
+  if (passwordLength < 8 || passwordLength > 128) {
     alert(
       "Passwords must be at least 8 characters and no more than 128 characters"
     );
     return;
   }
+  //prompts are in generatePassword because they are supposed to occur after generatebtn is clicked
+  var lowercase = window.prompt(
+    "Does your password require lowercase letters? (Y/N)"
+  );
+  lowercase.toLowerCase();
+  var uppercase = window.prompt(
+    "Does your password require uppercase letters? (Y/N)"
+  );
+  uppercase.toLowerCase();
+  var numbers = window.prompt("Does your password require numbers? (Y/N)");
+  numbers.toLowerCase();
+  var symbols = window.prompt("Does your password require symbols? (Y/N)");
+  symbols.toLowerCase();
+
+  //need something to concat if prompt === y
+  //probably also need to convert user input toLowercase(); or toUppercase(); for best results
+  //possibly use a switch statement. could use if/elif/else
+
+
+  //use for loop to get random assortment of items from criteria for password
+   /*for (i = 0; i < passwordLength; i++) {
+    newPassword += data[Math.floor(Math.random() * passwordData.length)];
+  } */
 }
 
 // Write password to the #password input
-function writePassword(passwordData) {
+function writePassword() {
   var password = generatePassword();
-  for (i = 0; i < passwordLength; i++) {
-    newPassword += data[Math.floor(Math.random() * passwordData.length)];
-  }
+ 
   passwordText.value = password;
   var passwordText = document.querySelector("#password");
 }
 
 // Add event listener to generate button
+//generatePassword with prompts is called by writePassword
 generateBtn.addEventListener("click", writePassword);
